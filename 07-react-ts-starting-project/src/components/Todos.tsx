d71da74c2e7826e563df06abe11aa2004873df48
cayden.ngai <cayden.ngai@lalamove.com>
2022-05-20 17:39:58 +0800

    remove all files

--- 07-react-ts-starting-project/src/components/Todos.tsx
+++
@@ -1,9 +0,0 @@
-const Todos: React.FC<{items: string[]}> = (props) => {
-  return (
-    <ul>
-      {props.items.map(item => <li key={item}>{item}</li>)}
-    </ul>
-  )
-}
-
-export default Todos;
